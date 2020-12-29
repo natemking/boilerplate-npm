283bcb8903b3e37a46539e37605c29aa5382a1f3
Nate M King <72567521+natemking@users.noreply.github.com>
2020-12-29 11:46:01 -0500

    Lesson two

--- myApp.js
+++ myApp.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 const path = require('path');
-const VIEWS_DIR = path.resolve(__dirname, 'views')
+const VIEWS_DIR = path.resolve(__dirname, './views')
 const index = path.join(VIEWS_DIR, 'index.html')
 
 app.get('/', (req,res) => {
-    res.send(index)
+    res.sendFile(index)
     
 })
     
