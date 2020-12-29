9d1b7237f98f9c3dd7989cb68937526e2b86baf9
Nate M King <72567521+natemking@users.noreply.github.com>
2020-12-29 11:42:25 -0500

    Lesson two

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
-
+const path = require('path');
+const VIEWS_DIR = path.resolve(__dirname, './views')
+const index = path.join(VIEWS_DIR, 'index.html')
 
 app.get('/', (req,res) => {
-    res.send('Hello Express')
+    res.send(index)
 })
     
 
