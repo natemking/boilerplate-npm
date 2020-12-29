35b4ba7594c8772a6391a825f23e45b53c76edbf
Nate M King <72567521+natemking@users.noreply.github.com>
2020-12-29 12:02:28 -0500

    Lesson four

--- myApp.js
+++ myApp.js
@@ -5,11 +5,17 @@ const PUBLIC_DIR = path.resolve(__dirname, './public');
 const VIEWS_DIR = path.resolve(__dirname, './views')
 const index = path.join(VIEWS_DIR, 'index.html')
 
+const obj = {'message': 'Hello json'}
+
 app.use(express.static(PUBLIC_DIR));
 
 app.get('/', (req,res) => {
     res.sendFile(index)
 });
+
+app.get('json', (req, res) => {
+  res.sendFile(obj)
+})
     
 
 
