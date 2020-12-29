3574ec3cab8400522c52ed63441a920b6ed8b9db
Nate M King <72567521+natemking@users.noreply.github.com>
2020-12-29 11:53:56 -0500

    Lesson three

--- myApp.js
+++ myApp.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
 const path = require('path');
+const PUBLIC_DIR = path.resolve(__dirname, './public');
 const VIEWS_DIR = path.resolve(__dirname, './views')
 const index = path.join(VIEWS_DIR, 'index.html')
 
+app.use(app.static(PUBLIC_DIR));
+
 app.get('/', (req,res) => {
     res.sendFile(index)
-    
-})
+});
     
 
 
