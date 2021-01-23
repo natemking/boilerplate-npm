c7a5df3a0500acef974329ecfb91c1855a1cca7f
Nate M King <72567521+natemking@users.noreply.github.com>
2021-01-23 16:44:07 -0500

    Lesson one

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  console.log('connected');
+});
+
 
 let Person;
 
