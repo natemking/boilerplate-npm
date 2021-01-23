82885b35eeab6e7a6021724a6ea4e3fd79c1a51e
Nate M King <72567521+natemking@users.noreply.github.com>
2021-01-23 17:55:47 -0500

    lesson two

--- myApp.js
+++ myApp.js
@@ -1,14 +1,20 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-  console.log(`Mongoose ready state is: ${mongoose.connection.readyState}`).catch((err) => {
-    console.log(err);
+const { Schema } = mongoose;
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true }).then((err) => {
+  console.log(`Mongoose ready state is: ${mongoose.connection.readyState}`)
   })
-})
+
+const personSchema = new Schema({
+  name: { type: String, required: true},
+  age: Number,
+  favoriteFoods: [String]
+}, { timestamps: true });
 
 
-let Person;
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
